314a20ae7b9c4d16b0ec43e28c8cfaa55b93481a
leonormsousa <20341608+leonormsousa@users.noreply.github.com>
2019-11-26 14:08:09 +0000

    Corrected header and footer

---
+++ templates/common/footer.php
@@ -0,0 +1,7 @@
+        <footer>
+            <h3>Amazing Rentals</h3>
+            <h5>All Rights Reserved.</h5>
+            <h5>João Praça, Leonor Sousa & Sílvia Rocha</h5>
+        </footer>
+    </body>
+</html>
---
+++ templates/common/search.php
@@ -0,0 +1,5 @@
+<form id="search" method="POST" action="/action_page.php">
+    <img src="images/glass.png" alt="magnifying glass" width="20"/>
+    <input type="search" name="q">
+    <input type="submit" value="Search">
+</form>
