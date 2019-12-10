1fcee39b840d4db1da61388876dc726095fb0de3
jarpraca <jarpraca@gmail.com>
2019-12-10 15:08:31 +0000

    added dates to search

--- templates/common/search.php
+++ templates/common/search.php
@@ -1,8 +1,9 @@
 <form id="search" method="POST" action="/action_page.php">
     <img src="images/glass.png" alt="magnifying glass" width="20"/>
-    <input class="search_input" type="search" name="q" placeholder="City, Country or Hotel">
-    <!--<input class="search_input" type="date" name="dateFrom">
-    <p>to</p>
-    <input class="search_input" type="date" name="dateTo">-->
+    <input class="search_input" type="search" name="q" placeholder="City, Country or Hotel" id="search_text_input">
+    <a> From: </a>
+    <input class="search_input" type="date" name="dateFrom" id="dateFrom">
+    <a> To: </a>
+    <input class="search_input" type="date" name="dateTo" id="dateTo">
     <input type="submit" value="Go">
 </form>
