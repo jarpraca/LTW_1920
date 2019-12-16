e64cbf982528b79b1e82c6c68fc9bbd2b67b16b3
jarpraca <jarpraca@gmail.com>
2019-12-16 11:46:59 +0000

    Added map to list reservations

--- templates/properties/viewSearchProperty.php
+++ templates/properties/viewSearchProperty.php
@@ -1,50 +1,50 @@
 <section class="propertySearch">
-        <?php 
-            include_once("database/connection.php");
-            include_once("database/habitations.php");
-            $pictures=getHabitationPictures($habitation['idHabitacao']);
+    <?php 
+        include_once("database/connection.php");
+        include_once("database/habitations.php");
+        $pictures=getHabitationPictures($habitation['idHabitacao']);
 
-            if ($pictures != null){
-                echo '<img src=' . $pictures[0]['urlImagem'] . ' alt=' . $pictures[0]['legenda'] . '>';
-            }
-            else{
-                echo '<img src="images/ownerPicture.jpg" alt="Habitation Picture">';
-            }
+        if ($pictures != null){
+            echo '<img src=' . $pictures[0]['urlImagem'] . ' alt=' . $pictures[0]['legenda'] . '>';
+        }
+        else{
+            echo '<img src="images/ownerPicture.jpg" alt="Habitation Picture">';
+        }
 
-            $total = $habitation['precoNoite']*$days+$habitation['taxaLimpeza'];
+        $total = $habitation['precoNoite']*$days+$habitation['taxaLimpeza'];
 
-            $comments = getComments($habitation['idHabitacao']);
-            $cleaning=0;
-            $location=0;
-            $value=0;
-            $check_in=0;
-            $rating=0;
-            $n=0;
-            foreach($comments as &$comment){
-                $cleaning+= $comment['limpeza'];
-                $location+= $comment['localizacao'];
-                $value+= $comment['valor'];
-                $check_in += $comment['checkIn'];
-                $n++;
-            }
-            if ($n!=0){
-                $location=$location/$n;
-                $cleaning=$cleaning/$n;
-                $value=$value/$n;
-                $check_in=$check_in/$n;
-                $rating=($location+$cleaning+$value+$check_in)/4;
-            }
-        ?>
-        
-        <div class="rightText">
-            <h3><?=getNameType($habitation['idTipo'])['nome']?></h3>
-            <h1><?=$habitation['nome']?></h1>
-            <p class="darkBlue">No. Rooms: <?=$habitation['numQuartos']?></p>  
-            <p class="darkBlue">No. Max Guests: <?=$habitation['maxHospedes']?></p>  
-            <div class="horizontalElements">
-                <h3 id="rating"><?=$rating?> (<?=$n?> users)</h3>
-                <h3>Total: <?=$total?>€</h3> 
-                <a href="viewProperty.php?id=<?=$habitation['idHabitacao']?>&dateFrom=<?=$_GET['dateFrom']?>&dateTo=<?=$_GET['dateTo']?>" class="submit"><p>View</p></a>
-            </div>
+        $comments = getComments($habitation['idHabitacao']);
+        $cleaning=0;
+        $location=0;
+        $value=0;
+        $check_in=0;
+        $rating=0;
+        $n=0;
+        foreach($comments as &$comment){
+            $cleaning+= $comment['limpeza'];
+            $location+= $comment['localizacao'];
+            $value+= $comment['valor'];
+            $check_in += $comment['checkIn'];
+            $n++;
+        }
+        if ($n!=0){
+            $location=$location/$n;
+            $cleaning=$cleaning/$n;
+            $value=$value/$n;
+            $check_in=$check_in/$n;
+            $rating=($location+$cleaning+$value+$check_in)/4;
+        }
+    ?>
+    
+    <div class="rightText">
+        <h3><?=getNameType($habitation['idTipo'])['nome']?></h3>
+        <h1><?=$habitation['nome']?></h1>
+        <p class="darkBlue">No. Rooms: <?=$habitation['numQuartos']?></p>  
+        <p class="darkBlue">No. Max Guests: <?=$habitation['maxHospedes']?></p>  
+        <div class="horizontalElements">
+            <h3 id="rating"><?=$rating?> (<?=$n?> users)</h3>
+            <h3>Total: <?=$total?>€</h3> 
+            <a href="viewProperty.php?id=<?=$habitation['idHabitacao']?>&dateFrom=<?=$_GET['dateFrom']?>&dateTo=<?=$_GET['dateTo']?>" class="submit"><p>View</p></a>
         </div>
+    </div>
 </section>
--- templates/properties/viewShortReservation.php
+++ templates/properties/viewShortReservation.php
@@ -1,43 +1,40 @@
 <section class="reservationShort">
-    <a href="viewProperty.php?id=<?=$reservation['idHabitacao']?>">
-        <?php 
-            include_once("database/connection.php");
-            include_once("database/habitations.php");
-            $habitation=getHabitationById($reservation['idHabitacao']);
-            $pictures=getHabitationPictures($habitation['idHabitacao']);
-            if ($pictures != null){
-                echo '<img src=' . $pictures['urlImagem'] . 'alt=' . $pictures['legenda'] . '>';
-            }
-            else{
-                echo '<img src="images/ownerPicture.jpg"  alt="Habitation Picture">';
-            }
+    <?php 
+        include_once("database/connection.php");
+        include_once("database/habitations.php");
+        $habitation=getHabitationById($reservation['idHabitacao']);
+        $pictures=getHabitationPictures($habitation['idHabitacao']);
+        if ($pictures != null){
+            echo '<img src=' . $pictures['urlImagem'] . 'alt=' . $pictures['legenda'] . '>';
+        }
+        else{
+            echo '<img src="images/ownerPicture.jpg"  alt="Habitation Picture">';
+        }
+    ?>
+    <div class="rightText">
+        <h3><?=getNameType($habitation['idTipo'])['nome']?></h3>
+        <h1><?=$habitation['nome']?></h1>
+        <h4><?=getStateName($reservation['idEstado'])?></h4>
+        <p><?=$reservation['dataCheckIn']?> -> <?=$reservation['dataCheckOut']?></p>
+        <?php
+            $datetime1 = strtotime($reservation['dataCheckIn'] . ' 00:00:00');
+            $datetime2 = strtotime($reservation['dataCheckOut'] . ' 00:00:00');
+            
+            $secs = $datetime2 - $datetime1;// == <seconds between the two times>
+            $days = $secs / 86400;
+            $total = $habitation['precoNoite']*$days+$habitation['taxaLimpeza'];
         ?>
-        <div class="rightText">
-            <h3><?=getNameType($habitation['idTipo'])['nome']?></h3>
-            <h1><?=$habitation['nome']?></h1>
-            <h4><?=getStateName($reservation['idEstado'])?></h4>
-            <p><?=$reservation['dataCheckIn']?> -> <?=$reservation['dataCheckOut']?></p>
+        <div class="horizontalElements">
+            <h3>Total: <?=$total?>€</h3>
             <?php
-                $datetime1 = strtotime($reservation['dataCheckIn'] . ' 00:00:00');
-                $datetime2 = strtotime($reservation['dataCheckOut'] . ' 00:00:00');
-                
-                $secs = $datetime2 - $datetime1;// == <seconds between the two times>
-                $days = $secs / 86400;
-                $total = $habitation['precoNoite']*$days+$habitation['taxaLimpeza'];
+                $state = $reservation['idEstado'];
+                if ($state == 0)
+                    echo '<a href=#>" class="submit"><p>Cancel</p></a>';
+                else if ($state == 1){
+                    echo '<a href="viewProperty.php?id=' . $habitation['idHabitacao'] . '#comment_form" class="submit"><p>Comment</p></a>';
+                }
             ?>
-            <div class="horizontalElements">
-                <h3>Total: <?=$total?>€</h3>
-                <?php
-                    $state = $reservation['idEstado'];
-                    print_r($state);
-                    if ($state == 0)
-                        echo '<a href=#>" class="submit"><p>Cancel</p></a>';
-                    else if ($state == 1){
-                        echo '<a href="viewProperty.php?id=' . $habitation['idHabitacao'] . '#comment_form" class="submit"><p>Comment</p></a>';
-                    }
-                ?>
-                
-            </div>
+            
         </div>
-    </a>
+    </div>
 </section>
